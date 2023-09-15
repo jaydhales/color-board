cb25e78b220be4df639ba316d57510386eba1054
james-o <jaydhales@gmail.com>
2023-09-15 12:15:42 +0100

    init hardhat

---
+++ Contract/contracts/Cell.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract BoardColor {
+    address owner;
+    uint8[7][5] private board;
+    enum Color {
+        white,
+        black,
+        red,
+        blue
+    }
+
+    constructor() {
+        uint8[7][5] storage _board = board;
+        for (uint8 i; i < _board.length; i++) {
+            for (uint8 j; j < _board[i].length; j++) {
+                _board[i][j] = _random(abi.encodePacked(i, j));
+            }
+        }
+        owner = msg.sender;
+    }
+
+    function _random(bytes memory _seed) internal pure returns (uint8) {
+        uint256 _rand = uint256(keccak256(_seed));
+        return (uint8(_rand) % 4) + 1;
+    }
+
+    function setCellColor(uint8 x, uint8 y, Color _color) public {
+        require(msg.sender == owner, "Not Owner");
+        board[x][y] = uint8(_color) + 1;
+    }
+
+    function getCellColor(
+        uint8 x,
+        uint8 y
+    ) external view returns (string memory) {
+        return _getColor(board[x][y]);
+    }
+
+    function _getColor(uint8 _id) internal pure returns (string memory) {
+        if (_id == 2) return "black";
+        if (_id == 3) return "red";
+        if (_id == 4) return "blue";
+        return "white";
+    }
+}
---
+++ Contract/scripts/deploy.ts
@@ -0,0 +1,21 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const board = await ethers.deployContract("BoardColor");
+
+  await board.waitForDeployment();
+
+  console.log(`Board deployed to ${board.target}`);
+
+  const color1 = await board.getCellColor(6, 4);
+  const color2 = await board.getCellColor(1, 4);
+
+  console.log({ color1, color2 });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
